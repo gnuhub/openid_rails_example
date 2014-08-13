83e1a8a5023308f368ea328b859f3b93eb7927ea
gnuhub <gnuhub@gmail.com>
2014-08-13 21:31:45 +0800

    backup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
+require "openid"
+require 'openid/extensions/sreg'
+require 'openid/extensions/pape'
+require 'openid/store/filesystem'
+
+
+
+
 class WelcomeController < ApplicationController
   def index
+  	openid_cusumber = OpenID::Consumer.new(session,OpenID::Store::Filesystem.new("#{ENV['HOME']}/tmp/openid"))
+  	#logger.info openid_cusumber.inspect
+  	r = openid_cusumber.begin "http://gnuhub.pip.verisignlabs.com/"
+  	logger.info r.inspect
+  	#logger.info r.status
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ OpenidTest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#index'
+  get "welcome/index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
